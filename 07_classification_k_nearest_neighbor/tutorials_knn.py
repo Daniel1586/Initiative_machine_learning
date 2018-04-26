c218a6a24df9a413dfbc1e62a269399486d396c6
litao <ltlitao717@126.com>
2018-04-26 16:12:20 +0800

    修正KNN算法程序, 添加算法输入数据集iris

--- 07_classification_k_nearest_neighbor/tutorials_knn.py
+++ 07_classification_k_nearest_neighbor/tutorials_knn.py
@@ -4,6 +4,9 @@
 """
 @file: tutorials_knn.py
 """
+import csv
+import random
+import operator
 import numpy as np
 import pandas as pd
 
@@ -15,12 +18,12 @@ def load_data(file):
     return dataset
 
 
-# 数据集划分训练集和测试集(当前设置测试集仅1个数据)
+# 数据集划分训练集和测试集
 # 返回数据类型为n维数组格式
 def split_dataset(dataset, test_size=1):
     cols = dataset.columns.tolist()
     data_idx = dataset.index.tolist()
-    test_idx = data_idx[-test_size:]
+    test_idx = random.sample(data_idx, test_size)
     train_idx = [i for i in data_idx if i not in test_idx]
 
     # 计算训练集, 转化为n维数组格式
@@ -36,90 +39,81 @@ def split_dataset(dataset, test_size=1):
     return train_datas, train_label, test_datas, test_label
 
 
-# 分类算法--朴素贝叶斯
-class NaiveBayes:
+# 分类算法--KNN
+class KNearestNeighbor(object):
 
     def __init__(self):
-        self.classes = None
-        self.x = None
-        self.y = None
-        self.paras = []    # 存储数据集中每个特征中每个特征值出现概率
+        pass
 
-    # 计算不同类别不同特征不同特征值的条件概率
-    def fit(self, x, y):
-        self.x = x
-        self.y = y
-        self.classes = np.unique(y)
+    # 计算距离度量
+    @staticmethod
+    def calc_distance(traindata, testdata):
 
-        # 遍历所有类别
-        for i in range(len(self.classes)):
-            c = self.classes[i]
-            c_idx = np.where(y == c)
-            c_set = x[c_idx[0]]
-            self.paras.append([])
+        distance = np.sqrt(np.sum(np.square(traindata-testdata)))
 
-            # 遍历相同类别不同特征
-            for j in range(c_set.shape[1]):
-                c_para = {}
-                c_attr = np.unique(c_set[:, j])
+        return distance
 
-                # 遍历相同特征不同特征值
-                for attr_value in c_attr:
-                    attr_value_num = c_set[c_set[:, j] == attr_value].shape[0]
-                    attr_value_pro = attr_value_num/c_set.shape[0]
-                    c_para[attr_value] = attr_value_pro
-                self.paras[i].append(c_para)
+    # 计算距离最近的k个样本
+    def get_neighbors(self, datas, label, test, k):
+        distances = []
+        # 测试样本和训练集中每个样本计算距离
+        for i in range(len(datas)):
+            dist = self.calc_distance(datas[i], test)
+            distances.append((datas[i], dist, label[i]))
+        distances.sort(key=operator.itemgetter(1))
 
-    # 计算类别先验概率
-    def calc_prior_prob(self, c):
-        c_idx = np.where(self.y == c)
-        c_set = self.x[c_idx[0]]
-        prior = c_set.shape[0]/self.x.shape[0]
+        neighbors = []
+        for j in range(k):
+            neighbors.append(distances[j][2])
 
-        return prior
+        return neighbors
 
-    # 单个样本数据分类
-    def classify(self, sample):
-        posteriors = []
+    # 按多数投票结果归类
+    @staticmethod
+    def get_class(neighbors):
+        classes = {}
+        for i in range(len(neighbors)):
+            class_i = neighbors[i][0]
+            if class_i in classes.keys():
+                classes[class_i] += 1
+            else:
+                classes[class_i] = 1
 
-        # 遍历所有类别,计算后验概率
-        for i in range(len(self.classes)):
-            c = self.classes[i]
-            prior = self.calc_prior_prob(c)
-            posterior = prior
+        # 类别数目降序排列
+        class_sorted = sorted(classes.items(), key=operator.itemgetter(1), reverse=True)
+        class_vote = class_sorted[0][0]
 
-            # 遍历所有特征的特征值
-            for j, params in enumerate(self.paras[i]):
-                sample_attr = sample[j]     # 预测样本第j个特征的值
-                prob = params.get(sample_attr)
-                posterior *= prob
+        return class_vote
 
-            posteriors.append(posterior)
+    # 计算测试样本准确率
+    @staticmethod
+    def get_accuracy(test_label, predictions):
+        correct = 0
+        for i in range(len(test_label)):
+            if test_label[i][-1] == predictions[i]:
+                correct += 1
+        print('预测准确率: {0}/{1}'.format(correct, len(test_label)))
+        acc = correct/len(test_label)
 
-        # 后验概率排序,找出最大后验概率
-        idx_of_max = np.argmax(posteriors)
-        max_value = posteriors[idx_of_max]
-        max_class = self.classes[idx_of_max]
-        print("The max posterior prob: ", max_value)
-        print("Classes by Naive Bayes: ", max_class)
+        return acc
 
-        return max_class
+    # 测试样本分类
+    def run(self, train_datas, train_label, test_datas, test_label, k=3):
+        predictions = []
+        # 遍历测试样本
+        for i in range(len(test_datas)):
+            neighbors = self.get_neighbors(train_datas, train_label, test_datas[i], k)
+            test_class = self.get_class(neighbors)
+            predictions.append(test_class)
 
-    # 对数据集进行类别预测
-    def predict(self, x):
-        y_predict = []
-        for sample in x:
-            y = self.classify(sample)
-            y_predict.append(y)
-
-        return np.array(y_predict)
+        accuracy = self.get_accuracy(test_label, predictions)
+        print('Accuracy: ' + repr(accuracy*100) + '%')
 
 
 if __name__ == "__main__":
-    filename = "dataset.csv"
+    filename = "dataset_iris.csv"
     data_set = load_data(filename)
-    x_train, y_train, x_test, y_test = split_dataset(data_set, test_size=1)
+    x_train, y_train, x_test, y_test = split_dataset(data_set, test_size=45)
 
-    clf = NaiveBayes()
-    clf.fit(x_train, y_train)
-    y_pred = clf.predict(x_test)
+    clf = KNearestNeighbor()
+    clf.run(x_train, y_train, x_test, y_test, k=3)
